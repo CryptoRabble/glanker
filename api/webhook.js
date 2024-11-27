579bd4709aeda667505b8192d1b6efcbb38257a3
CryptoRabble <cryptorabble@gmail.com>
2024-11-27 21:10:50 +1030

    dafhg

--- api/webhook.js
+++ api/webhook.js
@@ -146,7 +146,8 @@ async function generateTokenDetails(posts) {
        - Do not use the letters 'Q', 'X', and 'Z' too much
        - Do not use any existing popular memecoin names in the output
        - The name should be a real word
-       - The Ticker should be the same as the name`
+       - The name can be 1-2 words
+       - Try and make the ticker a fun abbreviation of the name, if possible. If not, it should be the same as the name`
      }]
    });
 
