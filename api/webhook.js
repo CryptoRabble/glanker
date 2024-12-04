8e75efc2ad909732ef82717e3b73e51b78374d36
CryptoRabble <cryptorabble@gmail.com>
2024-12-05 09:54:57 +1030

    test 1

--- api/webhook.js
+++ api/webhook.js
@@ -112,7 +112,7 @@ async function checkUserScore(fid) {
    const userScore = response.users?.[0]?.experimental?.neynar_user_score || 0;
    
    console.log('User score for FID:', fid, 'Score:', userScore);
-   return userScore >= 0.76;
+   return userScore >= 0.16;
  } catch (error) {
    console.error('Error checking user score:', error);
    return false;
