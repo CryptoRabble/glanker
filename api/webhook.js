f5b4a31f66063b4ec9dfb8bee404488cc036afd8
CryptoRabble <cryptorabble@gmail.com>
2024-12-08 21:04:27 +1030

    spirit

--- api/webhook.js
+++ api/webhook.js
@@ -3,7 +3,7 @@ import { NeynarAPIClient, Configuration } from "@neynar/nodejs-sdk";
 import { Anthropic } from '@anthropic-ai/sdk';
 import axios from 'axios';
 import crypto from 'crypto';
-import { safeRedisGet, safeRedisSet } from '../utils/redis';
+import { safeRedisGet, safeRedisSet } from './utils/redis.js';
 
 // Initialize clients
 init(process.env.AIRSTACK_API_KEY);
